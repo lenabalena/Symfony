d9bf8a5eea1ba9b2e3bd589414a2fea71adc2ab6
Marilena Dobrovolschi <marilenadobrovolschi10@gmail.com>
2020-11-03 11:09:40 +0300

    Using twig

--- src/Controller/Lucky_Controller.php
+++ src/Controller/Lucky_Controller.php
@@ -1,21 +1,28 @@
 <?php
 // src/Controller/LuckyController.php
 namespace App\Controller;
-
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Psr\Log\LoggerInterface;
     
 
-class Lucky_Controller
+class Lucky_Controller extends AbstractController
 { 	/**
       *@Route("/lucky_number")
       **/
+
     public function number(): Response
     {
         $number = random_int(0, 100);
 
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
+        return $this->render('lucky_number.html.twig', [
+            'number' => $number,
+        ]);
     }
+       public function index( LoggerInterface $logger)
+{
+    $logger->info("Saying hello to marilena!");
+
+}
 }
